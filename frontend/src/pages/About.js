9d06d80616e3a0a7ee2f12f10b3a2195f050bf88
elvinhung <elvinhung@gmail.com>
2020-03-03 17:23:33 -0600

    Added Github API call to get issues

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -5,6 +5,7 @@ import SectionHeader from "../components/SectionHeader";
 import '../styles/About.css';
 
 const About = () => {
+  const purpose = "";
   const teamMembers = [
     {
       name: 'Alex Kim',
@@ -12,6 +13,7 @@ const About = () => {
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis. Habitant morbi tristique senectus et netus. Faucibus scelerisque eleifend donec pretium vulputate sapien. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas',
       commits: 0,
       issues: 0,
+      tests: 0,
       githubUser: 'KimchiBean',
       img: 'AlexKim.JPG',
     },
@@ -21,6 +23,7 @@ const About = () => {
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis. Habitant morbi tristique senectus et netus. Faucibus scelerisque eleifend donec pretium vulputate sapien. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas',
       commits: 0,
       issues: 0,
+      tests: 0,
       githubUser: 'phenomanun',
       img: 'ManunChopra.jpg',
     },
@@ -30,6 +33,7 @@ const About = () => {
       bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis. Habitant morbi tristique senectus et netus. Faucibus scelerisque eleifend donec pretium vulputate sapien. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas',
       commits: 0,
       issues: 0,
+      tests: 0,
       githubUser: 'elvinhung',
       img: 'ElvinHung.jpg',
     },
@@ -39,6 +43,7 @@ const About = () => {
       bio: '',
       commits: 0,
       issues: 0,
+      tests: 0,
       githubUser: 'rchand20',
       img: '',
     },
@@ -48,29 +53,61 @@ const About = () => {
       bio: '',
       commits: 0,
       issues: 0,
+      tests: 0,
       githubUser: 'Nithanth',
       img: '',
     }
   ];
-  const [contributors, setContributors] = useState(teamMembers);
 
-  function updateTeamMembers(data) {
-    data.forEach(developer => {
-      contributors.forEach(contributor => {
-        if (contributor.githubUser === developer.author.login) {
-          contributor.commits = developer.total;
-          console.log(contributor.commits);
-        }
+  const teamStats = {
+    commits: 0,
+    issues: 0,
+    tests: 0,
+  };
+
+  const [contributors, setContributors] = useState(teamMembers);
+  const [stats, setStats] = useState(teamStats);
+
+  function updateStats(data, type) {
+    if (type === 'commits') {
+      let commits = 0;
+      data.forEach(developer => {
+        contributors.forEach(contributor => {
+          if (contributor.githubUser === developer.author.login) {
+            contributor.commits = developer.total;
+            commits += developer.total;
+          }
+        });
       });
-    });
+      setStats((prevState) => ({...prevState, commits}));
+    } else {
+      let issues = 0;
+      data.forEach(issue => {
+        contributors.forEach(contributor => {
+          if (contributor.githubUser === issue.user.login) {
+            contributor.issues += 1;
+            issues++;
+          }
+        });
+      });
+      setStats((prevState) => ({...prevState, issues}));
+    }
     setContributors((prevState) => [...contributors]);
   }
 
-  function getContributors() {
+  function getStats() {
     fetch("https://api.github.com/repos/elvinhung/SoftwareLabApp/stats/contributors")
       .then((res) => res.json())
       .then((data) => {
-        updateTeamMembers(data);
+        updateStats(data, 'commits');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    fetch("https://api.github.com/repos/elvinhung/SoftwareLabApp/issues?state=all")
+      .then((res) => res.json())
+      .then((data) => {
+        updateStats(data, 'issues');
       })
       .catch((err) => {
         console.log(err);
@@ -78,12 +115,18 @@ const About = () => {
   }
 
   useEffect(() => {
-    getContributors();
+    getStats();
   }, []);
 
   return(
     <div className="page-container">
       <Header />
+      <SectionHeader name="About" />
+      <div className="section-body">
+        <p>
+
+        </p>
+      </div>
       <SectionHeader name="The Team" />
       <div className="member-section-container">
         {contributors.map(member => {
@@ -91,6 +134,10 @@ const About = () => {
             <MemberSection member={member} />
           );
         })}
+      </div>
+      <SectionHeader name="Team Stats" />
+      <div className="team-stats-container">
+        <div>{stats.commits} commits {stats.issues} issues</div>
       </div>
     </div>
   );
