e172a1fad7267d7f2bc8db3c35cfe0ca01898189
manunchopra <manunchopra@Manuns-MacBook-Pro.local>
2020-03-03 20:56:17 -0600

    added basic restaurant's script from Yelp API (1 restaurant, photo, reviews)

---
+++ backend/restaurantsAPI.py
@@ -0,0 +1,39 @@
+import requests
+import json
+api_key='9cScMoHChZwCT3W4Rr3IK2Szy8iDQAKKvmuK0OMqor5L10Oa3JDm-D_MFDvIimzqp40rnBNQ55Pi2xzRxYlSUMEJnFix-LDqryJr2WMYIA4mdOszmSUfU2cA1UZUXnYx'
+headers = {'Authorization': 'Bearer %s' % api_key}
+url = 'https://api.yelp.com/v3/businesses/search'
+params = {'term':'seafood','location':'New York City', 'limit':1}
+
+request = requests.get(url, params=params, headers=headers)
+
+parsed = json.loads(request.text)
+
+businesses = parsed["businesses"]
+
+for business in businesses:
+    print("Name:", business["name"])
+    print("Rating:", business["rating"])
+    print("Address:", " ".join(business["location"]["display_address"]))
+    print("Phone:", business["phone"])
+    print("\n")
+
+    id = business["id"]
+
+    photo = business["image_url"]
+    print("--- Photo Link ---")
+    print(photo+ "\n")
+
+    url="https://api.yelp.com/v3/businesses/" + id + "/reviews"
+
+    request = requests.get(url, headers=headers)
+
+    parsed = json.loads(request.text)
+
+    reviews = parsed["reviews"]
+
+    print("--- Reviews ---")
+
+    for review in reviews:
+        print("User:", review["user"]["name"], "Rating:", review["rating"], "Review:", review["text"], "\n")
+
