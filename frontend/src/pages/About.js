09629a641a8ebafa58128e39ccffe295810e559f
elvinhung <elvinhung@gmail.com>
2020-03-03 22:09:33 -0600

    Added tool images and github images with links to each

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,8 +4,33 @@ import MemberSection from '../components/MemberSection';
 import SectionHeader from "../components/SectionHeader";
 import '../styles/About.css';
 
+const GithubStat = (props) => {
+  return (
+    <div className="github-stat">
+      <h2 align="center">{props.value}</h2>
+      <p align="center">{props.type}</p>
+    </div>
+  );
+}
+
+const ToolCard = (props) => {
+  return (
+    <a className="tool-link" href={props.link} target="_blank">
+      <div className="tool-card">
+        <div className="center">
+          <img className="tool-img" src={require("../assets/" + props.img)}/>
+        </div>
+        <p>{props.usage}</p>
+      </div>
+    </a>
+  );
+}
+
 const About = () => {
   const purpose = "";
+  const flaskUsage = "Our backend will be a RESTful API built with Flask. It will allow our front end to make HTTP Requests to our database.";
+  const awsUsage = "In order to host our website, we employed the use of AWS S3 for static website hosting."
+  const reactUsage = "We used React to implement the frontend of our application. It allows us to break up our website into different components and manage states of those components over time."
   const teamMembers = [
     {
       name: 'Alex Kim',
@@ -40,22 +65,22 @@ const About = () => {
     {
       name: 'Rishab Chander',
       major: 'ECE ᛫ Backend',
-      bio: '',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis. Habitant morbi tristique senectus et netus. Faucibus scelerisque eleifend donec pretium vulputate sapien. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas',
       commits: 0,
       issues: 0,
       tests: 0,
       githubUser: 'rchand20',
-      img: '',
+      img: 'ManunChopra.jpg',
     },
     {
       name: 'Nithanth Ram',
       major: 'ECE ᛫ Backend',
-      bio: '',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tristique et egestas quis. Habitant morbi tristique senectus et netus. Faucibus scelerisque eleifend donec pretium vulputate sapien. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas',
       commits: 0,
       issues: 0,
       tests: 0,
       githubUser: 'Nithanth',
-      img: '',
+      img: 'ManunChopra.jpg',
     }
   ];
 
@@ -128,16 +153,44 @@ const About = () => {
         </p>
       </div>
       <SectionHeader name="The Team" />
-      <div className="member-section-container">
-        {contributors.map(member => {
-          return (
-            <MemberSection member={member} />
-          );
-        })}
+      <div className="member-container">
+        <div className="member-section-container">
+          {contributors.map(member => {
+            return (
+              <MemberSection member={member} />
+            );
+          })}
+        </div>
       </div>
       <SectionHeader name="Team Stats" />
-      <div className="team-stats-container">
-        <div>{stats.commits} commits {stats.issues} issues</div>
+      <div className="centered-container">
+        <GithubStat type="commits" value={stats.commits} />
+        <GithubStat type="issues" value={stats.issues} />
+        <GithubStat type="unit tests" value={stats.tests} />
+      </div>
+      <SectionHeader name="Data" />
+      <SectionHeader name="Tools" />
+      <div className="centered-container wide">
+        <ToolCard
+          link="https://aws.amazon.com/"
+          usage={awsUsage}
+          img="AWS.png"
+        />
+        <ToolCard
+          link="https://reactjs.org/"
+          usage={reactUsage}
+          img="react.png"
+        />
+        <ToolCard
+          link="https://www.fullstackpython.com/flask.html"
+          usage={flaskUsage}
+          img="flask.jpg"
+        />
+      </div>
+      <div className="github-logo">
+        <a href="https://github.com/elvinhung/SoftwareLabApp" target="_blank">
+          <img width="200px" src={require("../assets/github.png")} alt="Github Repository" />
+        </a>
       </div>
     </div>
   );
