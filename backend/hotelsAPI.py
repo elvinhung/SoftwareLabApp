28028a031d4f86ec78cc031d77fe905672842fbc
rchand20 <rchand20@utexas.edu>
2020-03-27 18:44:46 -0500

    finalized hotel MongoDB document

--- backend/hotelsAPI.py
+++ backend/hotelsAPI.py
@@ -6,20 +6,14 @@ import os
 import requests
 import json
 
-#   import responses
-import googlemaps
-
-
-maps = googlemaps.Client('AIzaSyAV1O_tgq4HCmfC4yT7-a8gMKz0v2xSOqw')
-
 amadeus = Client(
     hostname='production',
-    client_id='D3dGGqI9fiGNXBAbGmCGhLMYeeue1YGg',
-    client_secret='H8M5btnJmg7Dke7g',
+    client_id='client_id',
+    client_secret='client_secret',
 )
 
 mongo = MongoClient()
-mongo_client = MongoClient('mongodb+srv://dbUser:adDJJ0ZG6O7VDdMz@softwarelabapp-hbwi6.mongodb.net/test?retryWrites=true&w=majority')
+mongo_client = MongoClient('connection_string')
 db = mongo_client.models
 hotels = db.hotels
 
@@ -29,52 +23,25 @@ f = open("hotel_locations.txt")
 for city_code in f:
     city_code = city_code.strip('\n')
     print(city_code)
-    city = amadeus.shopping.hotel_offers.get(cityCode = city_code, radius = '10', radiusUnit = 'MILE', ratings = '5,4')
+    city = amadeus.shopping.hotel_offers.get(cityCode = city_code, radius = '15', radiusUnit = 'MILE', ratings = '5,4')
     data = json.dumps(city.result)
     city_json = json.loads(data)
     city_dict = {}
     city_dict['hotels'] = []
-    print(len(city_json['data']))
     for p in city_json['data']:
-        if('rating' in p['hotel']):
-            city_dict['hotels'].append({
-            'name' : p['hotel']['name'],
-            'address' : p['hotel']['address'],
-            'stars' : p['hotel']['rating']
-            })
+        if('rating' in p['hotel'] and 'media' in p['hotel']):
+            temp = {}
+            temp['name'] = p['hotel']['name']
+            temp['address'] = p['hotel']['address']
+            temp['stars'] = p['hotel']['rating']
+            temp['image'] = p['hotel']['media'][0]['uri']
+            temp['contact'] = p['hotel']['contact']
+
+            if 'description' in p['hotel']:
+                temp['description'] = p['hotel']['description']['text']
+            
+            temp['location_id'] = city_code
+
+            city_dict['hotels'].append(temp)
         
     hotels.insert_many(city_dict['hotels'][0:3])
-
-
-# nyc = amadeus.shopping.hotel_offers.get(cityCode = 'NYC')
-# data1 = json.dumps(nyc.result)
-# nyc_json = json.loads(data1)
-# nyc_dict = {}
-# nyc_dict['hotels'] = []
-# for p in nyc_json['data']:
-#     if('rating' in p['hotel']):
-#         nyc_dict['hotels'].append({
-#         'name' : p['hotel']['name'], 
-#         'address' : p['hotel']['address'],
-#         'stars' : p['hotel']['rating']
-#         })
-
-# with open('nyc_hotel.json', 'w') as outfile:
-#     json.dump(nyc_dict, outfile)
-
-# sfo = amadeus.shopping.hotel_offers.get(cityCode = 'SAN')
-# data2 = json.dumps(sfo.result)
-# sfo_json = json.loads(data2)
-# sfo_dict = {}
-# sfo_dict['hotels'] = []
-# for p in sfo_json['data']:
-#     if('rating' in p['hotel']):
-#         sfo_dict['hotels'].append({
-#         'name' : p['hotel']['name'],
-#         'address' : p['hotel']['address'],
-#         'stars' : p['hotel']['rating']
-#         })
-
-# with open('sfo_hotel.json', 'w') as outfile:
-#     json.dump(sfo_dict, outfile)
-
