7b9fef6baf97cfd95baeff9b572b60b7e19d8073
phenomanun <chopra.manun@gmail.com>
2020-03-27 19:32:17 -0500

    finalized restaurant MongoDB document

--- backend/restaurantsAPI.py
+++ backend/restaurantsAPI.py
@@ -9,10 +9,10 @@ import requests
 import json
 
 
-yelp = YelpAPI(api_key)
+yelp = YelpAPI(yelp_api_key)
 
 mongo = MongoClient()
-mongo_client = MongoClient(client_key)
+mongo_client = MongoClient(mdb_connection_uri)
 db = mongo_client.models
 restaurants = db.restaurants
 
@@ -25,8 +25,8 @@ for city_code in f:
     response = yelp.search_query(term='food', location=city_code, sort_by='best_match', limit=3)
     data = json.dumps(response)
     restaurants_json = json.loads(data)
-    restaurants_dict = {}
-    restaurants_dict['businesses'] = []
+    city_dict = {}
+    city_dict['businesses'] = []
     for p in restaurants_json['businesses']:
         review = yelp.reviews_query(id=p['id'], sort_by='rating', limit=3)
         data = json.dumps(review)
@@ -39,69 +39,16 @@ for city_code in f:
             'text' : q['text'],
             'stars' : q['rating']
             })
-        restaurants_dict['businesses'].append({
-        'name' : p['name'],
-        'address' : p['location']['display_address'],
-        'stars' : p['rating'],
-        'reviews' : reviews_dict
-        })
+        doc_dict = {}
+        doc_dict['name'] = p['name'],
+        doc_dict['address'] = p['location']['display_address'],
+        doc_dict['stars'] = p['rating'],
+        doc_dict['image'] = p['image_url'],
+        doc_dict['contact'] = p['display_phone']
+        doc_dict['reviews'] = reviews_dict
+        doc_dict['location_id'] = city_code
 
-    restaurants.insert_many(restaurants_dict['businesses'])
+        city_dict['businesses'].append(doc_dict)
 
-# for business in businesses:
-#     print("Name:", business["name"])
-#     print("Rating:", business["rating"])
-#     print("Address:", " ".join(business["location"]["display_address"]))
-#     print("Phone:", business["phone"])
-#     print("\n")
-
-#     id = business["id"]
-
-#     photo = business["image_url"]
-#     print("--- Photo Link ---")
-#     print(photo+ "\n")
-
-#     url="https://api.yelp.com/v3/businesses/" + id + "/reviews"
-
-#     request = requests.get(url, headers=headers)
-
-#     parsed = json.loads(request.text)
-
-#     reviews = parsed["reviews"]
-
-#     print("--- Reviews ---")
-
-#     for review in reviews:
-#         print("User:", review["user"]["name"], "Rating:", review["rating"], "Review:", review["text"], "\n")
-
-# headers = {'Authorization': 'Bearer %s' % api_key}
-# url = 'https://api.yelp.com/v3/businesses/search'
-# params = {'term':'seafood','location':'New York City', 'limit':1}
-
-# request = requests.get(url, params=params, headers=headers)
-
-# nyc = json.loads(request.text)
-
-# with open('nyc_restaurant_final.json', 'w') as outfile:
-#     json.dump(nyc, outfile)
-
-# params = {'term':'seafood','location':'San Francisco', 'limit':1}
-
-# request = requests.get(url, params=params, headers=headers)
-
-# sfo = json.loads(request.text)
-
-# with open('sfo_restaurant_final.json', 'w') as outfile:
-#     json.dump(sfo, outfile)
-
-# params = {'term':'seafood','location':'London', 'limit':1}
-
-# request = requests.get(url, params=params, headers=headers)
-
-# lon = json.loads(request.text)
-
-# with open('lon_restaurant_final.json', 'w') as outfile:
-#     json.dump(lon, outfile)
-
-# businesses = sfo["businesses"]
+    restaurants.insert_many(city_dict['businesses'])
 
