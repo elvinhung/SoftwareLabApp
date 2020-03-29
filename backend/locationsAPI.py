3cc5377a689e6e7f38b645457481e07358a9b2f2
Nithanth <nithanth.ram@utexas.edu>
2020-03-29 00:17:52 -0500

    added hotels and restaurants to location model

--- backend/locationsAPI.py
+++ backend/locationsAPI.py
@@ -2,7 +2,11 @@ import googlemaps
 from pymongo import MongoClient
 import os
 
+from pymongo.errors import BulkWriteError
+
 url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+API_KEY = 'AIzaSyBJ2lOAHkcMp6O6SpyeRNcQ0jtjLqGpZnE'
+connection_string = 'mongodb+srv://dbUser:adDJJ0ZG6O7VDdMz@softwarelabapp-hbwi6.mongodb.net/test?retryWrites=true&w=majority'
 
 os.chdir(r'/Users/Nithanth/SoftwareLabApp/backend')
 city_dict = {}
@@ -13,26 +17,32 @@ with open("city_locations.txt") as txt1, open("hotel_locations.txt") as txt2:
         city_dict[city_name] = city_code
 
 mongo = MongoClient()
-mongo_client = MongoClient('connection_string')
+mongo_client = MongoClient(connection_string)
 db = mongo_client.models
 locations = db.locations
+hotels = db.hotels
+restaurants = db.restaurants
 
-dblist = mongo_client.list_database_names()
-if "locations" in dblist:
-    print("DB check complete")
-
-
-gmaps = googlemaps.Client(key = "API_KEY_HERE")
-
+gmaps = googlemaps.Client(key = API_KEY)
+locations.delete_many({})
 db_array = []
 for key in city_dict:
     city_name = key.strip('\n')
-    location = gmaps.find_place(input = city_name, input_type = 'textquery', fields = ['photos', 'name'])
+    location = gmaps.find_place(input = city_name, input_type = 'textquery', fields = ['photos', 'name', 'geometry'])
     temp = {}
     temp['name'] = location['candidates'][0]['name']
     temp['photos'] = location['candidates'][0]['photos']
+    temp['latitude'] = location['candidates'][0]['geometry']['location']['lat']
+    temp['longitude'] = location['candidates'][0]['geometry']['location']['lng']
+    hotel_matches = hotels.find({"location_id" : city_dict[key]})
+    temp['hotels'] = list(hotel_matches)
+    restaurant_matches = restaurants.find({"location_id" : city_dict[key]})
+    temp['restaurants'] = list(restaurant_matches)
     temp['_id'] = city_dict[key]
     db_array.append(temp)
 
-x = locations.insert_many(db_array)
-#print(x.inserted_ids)
+try:
+    x = locations.insert_many(db_array)
+except BulkWriteError as bwe:
+    print(bwe.details)
+
