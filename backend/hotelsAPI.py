45ce679b4c820502dd793af0e8173ce3e1aa9307
Rishab Chander <rchand20@utexas.edu>
2020-04-20 21:53:38 -0500

    added swimming pools to hotel documents

--- backend/hotelsAPI.py
+++ backend/hotelsAPI.py
@@ -25,41 +25,40 @@ hotels.create_index([
 )
 
 
-# os.chdir(r"C:\Users\risha\Documents\Spring 20\SoftwareLabApp\backend")
-# f = open("restaurant_locations.txt")
+os.chdir(r"C:\Users\risha\Documents\Spring 20\SoftwareLabApp\backend")
+f = open("restaurant_locations.txt")
 
-# for city_code in f:
-#     city = city_code.strip('\n')
-#     print(city_code)
-#     cityArr = city.split(',')
-#     cityCode = cityArr[0]
-#     cityStr = cityArr[1]
-#     print(city_code)
-#     city = amadeus.shopping.hotel_offers.get(cityCode = cityCode, radius = '10', radiusUnit = 'MILE')
-#     data = json.dumps(city.result)
-#     city_json = json.loads(data)
-#     city_dict = {}
-#     city_dict['hotels'] = []
-#     for p in city_json['data']:
-#         if('rating' in p['hotel'] and 'media' in p['hotel'] and 'amenities' in p['hotel']):
-#             temp = {}
-#             temp['name'] = p['hotel']['name']
-#             temp['address'] = p['hotel']['address']
-#             temp['stars'] = p['hotel']['rating']
-#             temp['image'] = p['hotel']['media'][0]['uri']
-#             temp['contact'] = p['hotel']['contact']
+for city_code in f:
+    city = city_code.strip('\n')
+    print(city_code)
+    cityArr = city.split(',')
+    cityCode = cityArr[0]
+    cityStr = cityArr[1]
+    print(city_code)
+    city = amadeus.shopping.hotel_offers.get(cityCode = cityCode, radius = '10', radiusUnit = 'MILE')
+    data = json.dumps(city.result)
+    city_json = json.loads(data)
+    city_dict = {}
+    city_dict['hotels'] = []
+    for p in city_json['data']:
+        if('rating' in p['hotel'] and 'media' in p['hotel'] and 'amenities' in p['hotel']):
+            temp = {}
+            temp['name'] = p['hotel']['name']
+            temp['address'] = p['hotel']['address']
+            temp['stars'] = p['hotel']['rating']
+            temp['image'] = p['hotel']['media'][0]['uri']
+            temp['contact'] = p['hotel']['contact']
 
-#             if "SWIMMING_POOL" in p['hotel']['amenities']:
-#                 temp['swimming_pool'] = True
-#             else:
-#                 temp['swimming_pool'] = False
+            if "SWIMMING_POOL" in p['hotel']['amenities']:
+                temp['swimming_pool'] = True
+            else:
+                temp['swimming_pool'] = False
 
-#             if 'description' in p['hotel']:
-#                 temp['description'] = p['hotel']['description']['text']
+            if 'description' in p['hotel']:
+                temp['description'] = p['hotel']['description']['text']
             
-#             temp['location_id'] = cityCode
+            temp['location_id'] = cityCode
 
-#             city_dict['hotels'].append(temp)
+            city_dict['hotels'].append(temp)
         
-#     hotels.insert_many(city_dict['hotels'][0:3])
-s
+    hotels.insert_many(city_dict['hotels'][0:3])
