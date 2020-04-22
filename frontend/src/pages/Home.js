479bf0c7d09ffb5abdbb6b2a407d6e580b07370b
elvinhung <elvinhung@gmail.com>
2020-04-22 05:35:37 -0500

    Added carousel to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,73 @@
 import React from 'react';
 import '../styles/Home.css';
 import 'font-awesome/css/font-awesome.min.css';
+import denver from "../assets/denver.jpg";
+import london from "../assets/london.jpg";
+import nyc from "../assets/nyc.jpg";
+import PhotoCarousel from "../components/PhotoCarousel";
+import {MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBContainer, MDBView} from "mdbreact";
+
+const images = [
+  {
+    img: denver,
+    id: 'DEN'
+  },
+  {
+    img: london,
+    id: 'LON'
+  },
+  {
+    img: nyc,
+    id: 'NYC'
+  }
+];
+
+const HomePageCarousel = (props) => {
+  return (
+    <MDBContainer className="home-page-carousel-container">
+      <MDBCarousel
+        activeItem={1}
+        length={3}
+        showControls={true}
+        showIndicators={true}
+        className="z-depth-1 fill"
+        slide
+      >
+        <MDBCarouselInner className="fill">
+          {props.images.map((image, index) => (
+            <MDBCarouselItem itemId={index + 1} className="fill">
+              <MDBView className="fill">
+                <img
+                  className="d-block home-page-carousel-img"
+                  src={image.img}
+                  alt={image.id}
+                />
+              </MDBView>
+            </MDBCarouselItem>
+          ))}
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </MDBContainer>
+  );
+}
 
 const Home = () => {
   return(
-    <div className="home-page-container">
-      <div className="title-container">
-        <h1>Where to?</h1>
+    <div className="home-page-split-container">
+      <div className="home-page-split-left">
+        <div className="home-page-search">
+          <div className="title-container">
+            <h3>Discover new places and experience cuisines from around the world</h3>
+            <p>Search from locations, restaurants, and hotels</p>
+          </div>
+          <form className="home-page-form-container fill" action="/search?">
+            <input name="name" id="textSearch" className="home-search-bar" placeholder="Anywhere" type="text"/>
+            <button type="submit" className="home-search-btn">Search</button>
+          </form>
+        </div>
       </div>
-      <div className="form-container">
-        <form classname="search-form" action="/search?">
-          <input name="name" id="textSearch" className="search-bar" placeholder="Anywhere" type="text"/>
-          <button type="submit" className="search-btn"><i className="fa fa-search"></i></button>
-        </form>
+      <div className="home-page-split-right">
+        <HomePageCarousel images={images} />
       </div>
     </div>
   );
