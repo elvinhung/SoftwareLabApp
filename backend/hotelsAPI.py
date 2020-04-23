b3ab6217bc47ccc55b91bac4c741b1ee821be734
Rishab Chander <rchand20@utexas.edu>
2020-04-22 22:53:16 -0500

    added swimming pool field to hotel documents

--- backend/hotelsAPI.py
+++ backend/hotelsAPI.py
@@ -13,52 +13,51 @@ amadeus = Client(
 )
 
 mongo = MongoClient()
-mongo_client = MongoClient('Connection_String')
+mongo_client = MongoClient('mongodb+srv://dbUser:adDJJ0ZG6O7VDdMz@softwarelabapp-hbwi6.mongodb.net/models?retryWrites=true&w=majority')
 db = mongo_client.models
 hotels = db.hotels
 
-hotels.create_index([
-      ('name', 'text'),
-      ('location_id', 'text')
+db.hotels.create_index([
+      ("$**", 'text')
   ],
   name="search_index"
 )
 
 
-# os.chdir(r"C:\Users\risha\Documents\Spring 20\SoftwareLabApp\backend")
-# f = open("restaurant_locations.txt")
+os.chdir(r"C:\Users\risha\Documents\Spring 20\SoftwareLabApp\backend")
+f = open("restaurant_locations.txt")
 
-# for city_code in f:
-#     city = city_code.strip('\n')
-#     print(city_code)
-#     cityArr = city.split(',')
-#     cityCode = cityArr[0]
-#     cityStr = cityArr[1]
-#     print(city_code)
-#     city = amadeus.shopping.hotel_offers.get(cityCode = cityCode, radius = '10', radiusUnit = 'MILE')
-#     data = json.dumps(city.result)
-#     city_json = json.loads(data)
-#     city_dict = {}
-#     city_dict['hotels'] = []
-#     for p in city_json['data']:
-#         if('rating' in p['hotel'] and 'media' in p['hotel'] and 'amenities' in p['hotel']):
-#             temp = {}
-#             temp['name'] = p['hotel']['name']
-#             temp['address'] = p['hotel']['address']
-#             temp['stars'] = p['hotel']['rating']
-#             temp['image'] = p['hotel']['media'][0]['uri']
-#             temp['contact'] = p['hotel']['contact']
+for city_code in f:
+    city = city_code.strip('\n')
+    print(city_code)
+    cityArr = city.split(',')
+    cityCode = cityArr[0]
+    cityStr = cityArr[1]
+    print(city_code)
+    city = amadeus.shopping.hotel_offers.get(cityCode = cityCode, radius = '10', radiusUnit = 'MILE')
+    data = json.dumps(city.result)
+    city_json = json.loads(data)
+    city_dict = {}
+    city_dict['hotels'] = []
+    for p in city_json['data']:
+        if('rating' in p['hotel'] and 'media' in p['hotel'] and 'amenities' in p['hotel']):
+            temp = {}
+            temp['name'] = p['hotel']['name']
+            temp['address'] = p['hotel']['address']
+            temp['stars'] = p['hotel']['rating']
+            temp['image'] = p['hotel']['media'][0]['uri']
+            temp['contact'] = p['hotel']['contact']
 
-#             if "SWIMMING_POOL" in p['hotel']['amenities']:
-#                 temp['swimming_pool'] = True
-#             else:
-#                 temp['swimming_pool'] = False
+            if "SWIMMING_POOL" in p['hotel']['amenities']:
+                temp['swimming_pool'] = True
+            else:
+                temp['swimming_pool'] = False
 
-#             if 'description' in p['hotel']:
-#                 temp['description'] = p['hotel']['description']['text']
+            if 'description' in p['hotel']:
+                temp['description'] = p['hotel']['description']['text']
             
-#             temp['location_id'] = cityCode
+            temp['location_id'] = cityCode
 
-#             city_dict['hotels'].append(temp)
+            city_dict['hotels'].append(temp)
         
-#     hotels.insert_many(city_dict['hotels'][0:3])
+    hotels.insert_many(city_dict['hotels'][0:3])
